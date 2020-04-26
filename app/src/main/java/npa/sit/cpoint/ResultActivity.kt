fe58d8f74bcef4f33231ae0ce9ef52b226b456e9
Pattaya Eye-dee <pattaya.eyedee@mail.kmutt.ac.th>
2020-04-26 17:43:55 +0700

    - Add Login Page Constraints - Add result page - Add result page code

--- app/src/main/java/npa/sit/cpoint/ResultActivity.kt
+++ app/src/main/java/npa/sit/cpoint/ResultActivity.kt
@@ -2,6 +2,7 @@ package npa.sit.cpoint
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.KeyEvent
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_result.*
@@ -19,6 +20,11 @@ class ResultActivity : AppCompatActivity(){
 
         val user = mAuth!!.currentUser
 
+        result_emailData.text = user!!.email
+        result_uidData.text = user!!.uid
+
+
+
         logout_logoutBtn.setOnClickListener{
             if(mAuth!=null){
                 mAuth!!.signOut()
@@ -30,4 +36,16 @@ class ResultActivity : AppCompatActivity(){
 
     }
 
+    override fun onStart() {
+        super.onStart()
+        mAuth!!.addAuthStateListener { mAuthListener }
+    }
+    override fun onStop() {
+        super.onStop()
+        if (mAuthListener != null) { mAuth!!.removeAuthStateListener { mAuthListener } }
+    }
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK) { moveTaskToBack(true) }
+        return super.onKeyDown(keyCode, event)
+    }
 }
