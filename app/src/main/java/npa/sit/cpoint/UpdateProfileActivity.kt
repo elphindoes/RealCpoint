94b028b17860446bcfa3e3459f23e7c013735582
Pattaya Eye-dee <pattaya.eyedee@mail.kmutt.ac.th>
2020-04-29 23:29:48 +0700

    - add profile. - add Database

--- app/src/main/java/npa/sit/cpoint/UpdateProfileActivity.kt
+++ app/src/main/java/npa/sit/cpoint/UpdateProfileActivity.kt
@@ -9,16 +9,17 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_update_profile.*
 
 
-class UpdateProfileActivity : AppCompatActivity(){
+class UpdateProfileActivity : AppCompatActivity() {
     var mAuth: FirebaseAuth? = null
-    var mDatabase : FirebaseDatabase? = null
+    var mDatabase: FirebaseDatabase? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,14 +28,39 @@ class UpdateProfileActivity : AppCompatActivity(){
         mAuth = FirebaseAuth.getInstance()
         mDatabase = FirebaseDatabase.getInstance()
 
-        var userId = mAuth!!.currentUser
-        var userRef = mDatabase!!.reference
+        var userId = mAuth!!.currentUser!!.uid
+        var userRef = mDatabase!!.reference.child("Users").child(userId)
+
+        userRef.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                finish()
+            }
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                var display_name = dataSnapshot.child("display_name").value.toString()
+                var email = dataSnapshot.child("email").value.toString()
+                var status = dataSnapshot.child("status").value.toString()
+                var name = dataSnapshot.child("name").value.toString()
+                var surname = dataSnapshot.child("surname").value.toString()
+                var image = dataSnapshot.child("image").value.toString()
+
+                textViewProfile.text = display_name
+                emailView2.text = email
+                statusView2.text = status
+                nameView2.text = name
+                surnameView2.text = surname
+
+                if (image != "Default") {
+                    Picasso.get().load(image).placeholder(R.drawable.defavatar)
+                        .into(update_imageView)
+
+                }
+
+            }
 
 
-
-        }
-
-
+        })
 
 
     }
+}
